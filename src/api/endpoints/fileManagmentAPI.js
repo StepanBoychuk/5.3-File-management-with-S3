f133a866fe3d69eaee864300004cff5b46d36410
Stepan Boychuk <stepanboichukk@gmail.com>
2024-04-17 15:14:30 +0300

    Fixed a bug with folders

--- src/api/endpoints/fileManagmentAPI.js
+++ src/api/endpoints/fileManagmentAPI.js
@@ -7,20 +7,22 @@ const {
   getFilesList,
   getFileUrl,
   deleteFile,
-  deleteFolder
+  deleteFolder,
 } = require("./../../services/s3.js");
 const cleanFilesList = require("./../../services/cleanFilesList.js");
 const checkRequestSize = require("../../middlwares/checkRequestSize.js");
-const checkIfFileExists = require('./../../middlwares/checkIfFileExists.js')
+const checkIfFileExists = require("./../../middlwares/checkIfFileExists.js");
 
 const filesAPI = Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-
 filesAPI.get("/api/filesystem", async (req, res) => {
-  const folder = req.body.folder + '/' || ''
+  let folder = "";
+  if (req.body.folder) {
+    folder = req.body.folder + "/";
+  }
   try {
     const { Contents } = await getFilesList(folder);
     res.send(cleanFilesList(Contents));
@@ -30,22 +32,32 @@ filesAPI.get("/api/filesystem", async (req, res) => {
   }
 });
 
-filesAPI.get("/api/filesystem/:filename", checkIfFileExists, async (req, res) => {
-  const folder = req.body.folder + '/' || ''
-  try {
-    res.send(await getFileUrl(req.params.filename, folder))
-  } catch (error) {
-    logger.error(error);
-    res.status(500).send(error.message);
+filesAPI.get(
+  "/api/filesystem/:filename",
+  checkIfFileExists,
+  async (req, res) => {
+    let folder = "";
+    if (req.body.folder) {
+      folder = req.body.folder + "/";
+    }
+    try {
+      res.send(await getFileUrl(req.params.filename, folder));
+    } catch (error) {
+      logger.error(error);
+      res.status(500).send(error.message);
+    }
   }
-});
+);
 
 filesAPI.post(
   "/api/filesystem",
   checkRequestSize,
   upload.single(),
   async (req, res) => {
-    const folder = req.body.folder + '/' || ''
+    let folder = "";
+    if (req.body.folder) {
+      folder = req.body.folder + "/";
+    }
     try {
       if (!req.file) {
         return res.status(400).send("No file uploaded");
@@ -68,31 +80,43 @@ filesAPI.post(
   }
 );
 
-filesAPI.delete("/api/filesystem/:filename", checkIfFileExists, async (req, res) => {
-  const folder = req.body.folder + '/' || ''
+filesAPI.delete(
+  "/api/filesystem/:filename",
+  checkIfFileExists,
+  async (req, res) => {
+    if (req.body.folder) {
+      const folder = req.body.folder + "/";
+    } else {
+      const folder = "";
+    }
+    try {
+      await deleteFile(req.params.filename, folder);
+      res.status(204).send();
+    } catch (error) {
+      logger.error(error);
+      res.status(500).send(error.message);
+    }
+  }
+);
+
+filesAPI.delete("/api/filesystem", async (req, res) => {
   try {
-    await deleteFile(req.params.filename, folder);
+    if (!req.body.folder) {
+      return res
+        .status(400)
+        .send(
+          `Please, add key 'folder' with value of folder name to request body`
+        );
+    }
+    if (ifObjectExists("", req.body.folder)) {
+      return res.status(404).send("There is no folder with this name");
+    }
+    deleteFolder(req.body.folder);
     res.status(204).send();
   } catch (error) {
     logger.error(error);
     res.status(500).send(error.message);
   }
 });
-
-filesAPI.delete("/api/filesystem", async (req, res) => {
-  try {
-    if (!req.body.folder) {
-      return res.status(400).send(`Please, add key 'folder' with value of folder name to request body`)
-    }
-    if (ifObjectExists('', req.body.folder)) {
-      return res.status(404).send('There is no folder with this name')
-    }
-    deleteFolder(req.body.folder)
-    res.status(204).send()
-  }catch(error){
-    logger.error(error)
-    res.status(500).send(error.message)
-  }
-})
 
 module.exports = filesAPI;
